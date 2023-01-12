9503ce2c065b192c752558c941f224e700e28997
zakve <zaklas.martin@gmail.com>
2023-01-12 12:30:13 +0100

    Add jest ts-jest supertest libs and test script

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,9 @@ import usageRouter from './routes/usage'
 app.use('/currencies', currencyRouter)
 app.use('/usage', usageRouter)
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'test') {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+}
+
+export default app
